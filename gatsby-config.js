f85b67e32195353d3fd8e821aed813c641d775eb
Felipe Homrich Melchior <fehmel@gmail.com>
2020-01-17 21:25:22 -0300

    feat: cleanup start files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Felipe Homrich Melchior`,
+    description: `Portfólio pertencente a Felipe Homrich Melchior, desenvolvedor e pesquisador de Segurança da Informação.`,
+    author: `@felipemelchior`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
